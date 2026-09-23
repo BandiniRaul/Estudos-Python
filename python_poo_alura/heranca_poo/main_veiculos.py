7907ec7265045cbb951d722ad5d1b038a9411799
bandiniraul <bandiniraul@gmail.com>
2026-09-23 16:39:07 -0300

    Algumas mudanças na estrutura do projeto e atividade de herança concluídas

---
+++ python_poo_alura/heranca_poo/main_veiculos.py
@@ -0,0 +1,19 @@
+from carro import Carro
+from moto import Moto
+
+
+carro_1 = Carro("Toyota", "Corolla", 4)
+carro_2 = Carro("Volkswagen", "Gol", 2)
+carro_3 = Carro("Honda", "Civic", 4)
+
+moto_1 = Moto("Honda", "CB 500", "Casual")
+moto_2 = Moto("Yamaha", "MT-03", "Esportiva")
+moto_3 = Moto("BMW", "R 1250 GS", "Espotiva")
+
+print(carro_1)
+print(carro_2)
+print(carro_3)
+print()
+print(moto_1)
+print(moto_2)
+print(moto_3)
