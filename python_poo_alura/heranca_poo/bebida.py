0ebb3f6a1f449da2a106744448288b68524572b2
bandiniraul <bandiniraul@gmail.com>
2026-09-24 08:31:43 -0300

    Nova biblioteca e atualizando o requirements

--- python_poo_alura/heranca_poo/bebida.py
+++ python_poo_alura/heranca_poo/bebida.py
@@ -1,4 +1,4 @@
-from modelos.cardapio.item_cardapio import ItemCardapio
+from item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
     def __init__(self,nome,preco,tamanho):
@@ -10,4 +10,3 @@ class Bebida(ItemCardapio):
 
     def aplicar_desconto(self):
         self._preco -= (self._preco * 0.08)
-        
